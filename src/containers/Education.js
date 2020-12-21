69ce88fc31f4c7b6524f0c9795e158a702111fbf
Md Alamin <mdalamin.eu@gmail.com>
2020-12-21 23:16:32 +0200

    update in Education

--- src/containers/Education.js
+++ src/containers/Education.js
@@ -28,12 +28,12 @@ export const PortButton = styled.a`
     padding: 5px 7px;
   }
 `;
-const PortTitle = styled.h2`
+const PortTitle = styled.h4`
   flex: 1;
   font-family: Gotham, Helvetica, Arial, sans-serif;
   text-transform: uppercase;
 
-  @media (max-width: 600px) {
+  @media (max-width: 300px) {
     text-align: center;
   }
 `;
@@ -48,31 +48,25 @@ class Education extends Component {
         
         <PortTitle>Centria University of Applied Sciences   <small>(Kokkola, Finland.)</small> </PortTitle> 
         
-        <p  style={{ 'font-size': '20px' }}>
+        <p  style={{ 'font-size': '15px' }}>
         <i>Bachelor of Engineering, Information Technology</i>
-        <h5>The course is aimed at the three major areas of Software Engineering, Network Engineering, and Industrial knowledge. 
-          The professional will know how to use efficiently the available resources of the global network of 
-          computers and have expertise in languages and programming environments for the development of software. 
-          The Technical provides software and technical procedures for the selection and use of data structures in the resolution of problems, 
-          concerning issues from the mechanisms of control or operational control of programmes.</h5>
+        <h6>The course is aimed at the three major areas of Software Engineering, Network Engineering, and Industrial knowledge. </h6>
           
-          <h5> <storng>They are part of the curriculum structure following disciplines:</storng></h5>
+          <h6> <storng>They are part of the curriculum structure following disciplines:</storng></h6>
 
-          <h5>Embeded System and PLC, Programming Languge C, C++, C#, Java and SQL, IOT, AWS
-          </h5>
+          <h6>Embeded System and PLC, Programming Languge C, C++, C#, Java and SQL, IOT, AWS
+          </h6>
         
         </p>
         <br/>
         <PortTitle>Chittagong Polytechnic Institute <small>(Chittagong, Bangladesh.)</small></PortTitle>
-        <p  style={{ 'font-size': '22px' }}>
+        <p  style={{ 'font-size': '15px' }}>
             <i>4 Years Diploma in Computer Engineering</i>
-            <h5>The degree is divided into three major areas of Computer Network, Software, and Hardware Engineering.
-          The methodology of teaching prioritizes the construction of knowledge through experimentation, 
-          the proposition of situations, problems, or scenarios over which should reflect academic, research, 
-          and devise solutions within the programming. </h5>
-          <h5>They are part of the curriculum structure following disciplines:</h5>
-            <h5>Embeded System and PLC, Programming Languge C, C++, C#, Java and SQL
-          </h5>
+            <h6>The degree is divided into three major areas of Computer Network, Software, and Hardware Engineering.</h6>
+          <h6>They are part of the curriculum structure following disciplines:</h6>
+            <h6>
+              <li>Embeded System and PLC,</li> Programming Languge C, C++, C#, Java and SQL
+          </h6>
         </p>
       </div>
     

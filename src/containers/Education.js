ef536701027133dd845471ead35710a5d94660bd
Md Alamin <mdalamin.eu@gmail.com>
2020-12-21 20:52:10 +0200

    Update Education

--- src/containers/Education.js
+++ src/containers/Education.js
@@ -50,12 +50,29 @@ class Education extends Component {
         
         <p  style={{ 'font-size': '20px' }}>
         <i>Bachelor of Engineering, Information Technology</i>
+        <h5>The course is aimed at the three major areas of Software Engineering, Network Engineering, and Industrial knowledge. 
+          The professional will know how to use efficiently the available resources of the global network of 
+          computers and have expertise in languages and programming environments for the development of software. 
+          The Technical provides software and technical procedures for the selection and use of data structures in the resolution of problems, 
+          concerning issues from the mechanisms of control or operational control of programmes.</h5>
+          
+          <h5> <storng>They are part of the curriculum structure following disciplines:</storng></h5>
+
+          <h5>Embeded System and PLC, Programming Languge C, C++, C#, Java and SQL
+          </h5>
         
         </p>
         <br/>
         <PortTitle>Chittagong Polytechnic Institute <small>(Chittagong, Bangladesh.)</small></PortTitle>
-        <p  style={{ 'font-size': '20px' }}>
+        <p  style={{ 'font-size': '22px' }}>
             <i>4 Years Diploma in Computer Engineering</i>
+            <h5>The degree is divided into three major areas of Computer Network, Software, and Hardware Engineering.
+          The methodology of teaching prioritizes the construction of knowledge through experimentation, 
+          the proposition of situations, problems, or scenarios over which should reflect academic, research, 
+          and devise solutions within the programming. </h5>
+          <h5>They are part of the curriculum structure following disciplines:</h5>
+            <h5>Embeded System and PLC, Programming Languge C, C++, C#, Java and SQL
+          </h5>
         </p>
       </div>
     
